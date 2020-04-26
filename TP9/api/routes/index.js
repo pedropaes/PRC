23544b0bc9b31af0ee0f44b7dc5707ba7e4b6e4d
pedropaes <pedropaes@gmail.com>
2020-04-27 00:18:05 +0100

    TP9

---
+++ TP9/api/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var Filmes = require('../controllers/filmes')
+
+router.get('/filmes', function(req, res) {
+  Filmes.getLista()
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem de filmes: ${e}`))
+});
+
+router.get('/filmes/:id/atores', function(req, res) {
+  Filmes.getAtoresDoFilme(req.params.id)
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem dos atores do filme ${req.params.id}: ${e}`))
+});
+
+router.get('/filmes/:id/generos', function(req, res) {
+  Filmes.getGenerosDoFilme(req.params.id)
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem dos géneros do filme ${req.params.id}: ${e}`))
+});
+
+router.get('/filmes/:id/personagens', function(req, res) {
+  Filmes.getPersonagensDoFilme(req.params.id)
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem dos personagens do filme ${req.params.id}: ${e}`))
+});
+
+router.get('/filmes/:id', function(req, res) {
+  Filmes.getFilme(req.params.id)
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem do filme ${req.params.id}: ${e}`))
+});
+
+router.get('/atores', function(req, res) {
+  Filmes.getAtores()
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem de filmes: ${e}`))
+});
+
+router.get('/atores/:id', function(req, res) {
+  Filmes.getAtor(req.params.id)
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem do filme ${req.params.id}: ${e}`))
+});
+
+module.exports = router;
